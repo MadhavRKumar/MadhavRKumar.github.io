09e7b9484c5dc57e467b7418b3565c7015f4091b
MadhavRKumar <MadhavRKumar@gmail.com>
2018-08-01 14:01:07 -0400

    added buttons and ending

--- projects/name/sketch.js
+++ projects/name/sketch.js
@@ -1,15 +1,24 @@
 var vowels = 'aeiou';
 var consonants = 'bcdfghjklmnpqrstvwxyz';
 var name = "";
+var wrongName = "";
 var showLine = true;
+var nameEntered = false;
+var isDone = false;
 var statement = "";
 
 const fontSize = 48;
 const typeSize = 32;
 const question = "What is your name?";
-function preload() {
+const stopTrying = "Ugh your name is too hard.\nI'll just call you ";
+const giveUp = "Well it's nice to meet you,\n";
 
-}
+var yesX;
+var noX;
+var buttonY;
+var buttonWidth = 200;
+var buttonHeight = typeSize * 2;
+
 
 function setup() {
     createCanvas(600, 400);
@@ -18,47 +27,75 @@ function setup() {
     textAlign(CENTER);
     strokeWeight(4);
     statement = question;
+
+    yesX = width / 4;
+    noX = 3 * width / 4;
+    buttonY = height * 0.6;
 }
 
 
 function draw() {
     background("#333333")
     noStroke();
-    
-    textSize(fontSize);
-    text(statement, width / 2, height * 0.25);
-    
-    textSize(typeSize);
-    text(name, width / 2, height * 0.5);
+    if (!isDone) {
+        fill("#efefef");
+        textSize(fontSize);
+        text(statement, width / 2, height * 0.25);
 
-    if (frameCount % 45 == 0) {
-        showLine = !showLine;
+        textSize(typeSize);
+        if (!nameEntered) {
+            text(name, width / 2, height * 0.5);
+        }
+
+        if (frameCount % 45 == 0) {
+            showLine = !showLine;
+        }
+
+        if (showLine && !nameEntered) {
+            stroke("#efefef");
+            line((width + textWidth(name)) / 2, height * 0.5 - typeSize, (width + textWidth(name)) / 2, height * 0.5 + typeSize / 2);
+        }
+
+        if (nameEntered) {
+            displayButtons();
+        }
     }
-
-    if (showLine) {
-        stroke("#efefef");
-        line((width + textWidth(name)) / 2, height * 0.5 - typeSize, (width + textWidth(name)) / 2, height * 0.5 + typeSize / 2);
+    else{
+        fill("#efefef");
+        text(giveUp + wrongName + "!", width/2, height/2);
     }
+}
 
+function mousePressed() {
+    if (isMouseInBox(yesX, buttonY, buttonWidth, buttonHeight) && nameEntered) {
+        isDone = true;
+    }
+    else if (isMouseInBox(noX, buttonY, buttonWidth, buttonHeight) && nameEntered) {
+        guess();
+    }
 }
 
 function keyPressed() {
     if (keyCode == BACKSPACE) {
         name = name.slice(0, -1);
     }
-    else if (keyCode == ENTER){
-        statement = messUpName(name) + "?";
-        name = "";
+    else if (keyCode == ENTER) {
+        guess();
+        nameEntered = true;
     }
 }
 
 function keyTyped() {
-    if (keyCode != BACKSPACE && keyCode != 0 && keyCode != ENTER) {
+    if (keyCode != BACKSPACE && keyCode != 0 && keyCode != ENTER && !nameEntered) {
 
         name += key;
     }
 }
 
+function guess() {
+    wrongName = messUpName(name);
+    statement = wrongName + "?";
+}
 
 function messUpName(n) {
     var numLetters = floor(random(1, 3));
@@ -87,9 +124,39 @@ function messUpName(n) {
             }
         }
     }
-    while (join(arr,"") == n);
+    while (join(arr, "") == n);
 
     return join(arr, "");
+}
 
 
+function displayButtons() {
+    rectMode(CENTER);
+
+
+    if (isMouseInBox(yesX, buttonY, buttonWidth, buttonHeight)) {
+        fill("#999999");
+    }
+    else {
+        fill("#efefef");
+    }
+    rect(yesX, buttonY, buttonWidth, buttonHeight, 100);
+
+    if (isMouseInBox(noX, buttonY, buttonWidth, buttonHeight)) {
+        fill("#999999");
+    }
+    else {
+        fill("#efefef");
+    }
+    rect(noX, buttonY, buttonWidth, buttonHeight, 100);
+
+    fill("#333333");
+    text("YES", yesX, (buttonY + typeSize * 0.4));
+
+    text("NO", noX, (buttonY + typeSize * 0.4));
+
+}
+
+function isMouseInBox(x, y, w, h) {
+    return ((mouseX < x + w / 2) && (mouseX > x - w / 2) && (mouseY < y + h / 2) && (mouseY > y - h / 2))
 }
